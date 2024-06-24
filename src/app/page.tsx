730bde0a7e85b92e8f271d3570bbc11b912ca151
vivartmedia <nanno70@yahoo.de>
2024-06-24 16:50:18 -0700

    update form field IDs and placeholders for clarity and consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -141,76 +141,106 @@ export default function Home() {
         <form onSubmit={handleSubmit} className="flex max-w-4xl flex-col gap-6 bg-slate-400 p-10 rounded-md">
           <div className="flex gap-10">
             <div className="flex w-full flex-col gap-6">
-              <div>
-                <div className="mb-2 flex items-center">
-                  <Label className="text-lg font-semibold" htmlFor="firstName" value="First Name" />
-                  <Tooltip content="Enter your first name. Max 100 characters." placement="right">
-                    <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
-                  </Tooltip>
+
+              {/* ------------------------------------------------------- */}
+              <div className="flex w-full  gap-6">
+                <div>
+                  <div className="mb-2 flex items-center">
+                    <Label className="text-lg " htmlFor="firstName" value="First Name" />
+                    <Tooltip content="Enter your first name. Max 100 characters." placement="right">
+                      <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
+                    </Tooltip>
+                  </div>
+                  <TextInput id="firstName" type="text" placeholder="First Name" required onChange={handleChange} value={formValues.firstName} />
+                  {formErrors.firstName && <p className="text-red-600">{formErrors.firstName}</p>}
                 </div>
-                <TextInput id="firstName" type="text" placeholder="First Name" required onChange={handleChange} value={formValues.firstName} />
-                {formErrors.firstName && <p className="text-red-600">{formErrors.firstName}</p>}
+
+                <div>
+                  <div className="mb-2 flex items-center">
+                    <Label className="text-lg " htmlFor="lastName" value="Last Name" />
+                    <Tooltip content="Enter your last name. Max 100 characters." placement="right">
+                      <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
+                    </Tooltip>
+                  </div>
+                  <TextInput id="lastName" type="text" placeholder="Last Name" required onChange={handleChange} value={formValues.lastName} />
+                  {formErrors.lastName && <p className="text-red-600">{formErrors.lastName}</p>}
+                </div>
+              </div>
+              {/* ------------------------------------------------------- */}
+
+
+
+              {/* ------------------------------------------------------- */}
+              <div className="flex w-full  gap-6">
+                <div>
+                  <div className="mb-2 flex items-center">
+                    <Label className="text-lg " htmlFor="email" value="Your email" />
+                    <Tooltip content="Enter a valid email address." placement="right">
+                      <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
+                    </Tooltip>
+                  </div>
+                  <TextInput id="email" type="email" placeholder="name@flowbite.com" required onChange={handleChange} value={formValues.email} />
+                  {formErrors.email && <p className="text-red-600">{formErrors.email}</p>}
+                </div>
+
+                <div>
+                  <div className="mb-2 flex items-center">
+                    <Label className="text-lg " htmlFor="dateOfBirth" value="Date of Birth" />
+                    <Tooltip content="Select your date of birth. Future dates are not allowed." placement="right">
+                      <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
+                    </Tooltip>
+                  </div>
+                  <TextInput id="dateOfBirth" type="date" placeholder="Date of Birth" required onChange={handleChange} value={formValues.dateOfBirth} />
+                  {formErrors.dateOfBirth && <p className="text-red-600">{formErrors.dateOfBirth}</p>}
+                </div>
+              </div>
+              {/* ------------------------------------------------------- */}
+
+
+
+           
+
+
+
+
+
+              {/* ------------------------------------------------------- */}
+              <div className="flex w-full  gap-6">
+                <div>
+                  <div className="mb-2 flex items-center">
+                    <Label className="text-lg " htmlFor="address" value="Address" />
+                    <Tooltip content="Enter your address. Max 100 characters." placement="right">
+                      <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
+                    </Tooltip>
+                  </div>
+                  <TextInput id="address" type="text" placeholder="Address" onChange={handleChange} value={formValues.address} />
+                  {formErrors.address && <p className="text-red-600">{formErrors.address}</p>}
+                </div>
+
+                <div>
+                  <div className="mb-2 flex items-center">
+                    <Label className="text-lg " htmlFor="phoneN" value="Phone number" />
+                    <Tooltip content="Enter your phone number in the format (123) 456-7890." placement="right">
+                      <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
+                    </Tooltip>
+                  </div>
+                  <TextInput id="phoneN" type="text" placeholder="Phone number" onChange={handleChange} value={formValues.phoneN} />
+                  {formErrors.phoneN && <p className="text-red-600">{formErrors.phoneN}</p>}
+                </div>
               </div>
 
-              <div>
-                <div className="mb-2 flex items-center">
-                  <Label className="text-lg font-semibold" htmlFor="lastName" value="Last Name" />
-                  <Tooltip content="Enter your last name. Max 100 characters." placement="right">
-                    <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
-                  </Tooltip>
-                </div>
-                <TextInput id="lastName" type="text" placeholder="Last Name" required onChange={handleChange} value={formValues.lastName} />
-                {formErrors.lastName && <p className="text-red-600">{formErrors.lastName}</p>}
-              </div>
 
-              <div>
-                <div className="mb-2 flex items-center">
-                  <Label className="text-lg font-semibold" htmlFor="email" value="Your email" />
-                  <Tooltip content="Enter a valid email address." placement="right">
-                    <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
-                  </Tooltip>
-                </div>
-                <TextInput id="email" type="email" placeholder="name@flowbite.com" required onChange={handleChange} value={formValues.email} />
-                {formErrors.email && <p className="text-red-600">{formErrors.email}</p>}
-              </div>
+              {/* ------------------------------------------------------- */}
 
-              <div>
-                <div className="mb-2 flex items-center">
-                  <Label className="text-lg font-semibold" htmlFor="dateOfBirth" value="Date of Birth" />
-                  <Tooltip content="Select your date of birth. Future dates are not allowed." placement="right">
-                    <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
-                  </Tooltip>
-                </div>
-                <TextInput id="dateOfBirth" type="date" placeholder="Date of Birth" required onChange={handleChange} value={formValues.dateOfBirth} />
-                {formErrors.dateOfBirth && <p className="text-red-600">{formErrors.dateOfBirth}</p>}
-              </div>
-            </div>
-            <div className="flex w-full flex-col gap-6">
-              <div>
-                <div className="mb-2 flex items-center">
-                  <Label className="text-lg font-semibold" htmlFor="address" value="Address" />
-                  <Tooltip content="Enter your address. Max 100 characters." placement="right">
-                    <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
-                  </Tooltip>
-                </div>
-                <TextInput id="address" type="text" placeholder="Address" onChange={handleChange} value={formValues.address} />
-                {formErrors.address && <p className="text-red-600">{formErrors.address}</p>}
-              </div>
 
-              <div>
-                <div className="mb-2 flex items-center">
-                  <Label className="text-lg font-semibold" htmlFor="phoneN" value="Phone number" />
-                  <Tooltip content="Enter your phone number in the format (123) 456-7890." placement="right">
-                    <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
-                  </Tooltip>
-                </div>
-                <TextInput id="phoneN" type="text" placeholder="Phone number" onChange={handleChange} value={formValues.phoneN} />
-                {formErrors.phoneN && <p className="text-red-600">{formErrors.phoneN}</p>}
-              </div>
 
-              <div>
+
+
+              {/* ------------------------------------------------------- */}
+              <div className="flex w-full  gap-6">
+                <div>
                 <div className="mb-2 flex items-center">
-                  <Label className="text-lg font-semibold" htmlFor="password" value="Password" />
+                  <Label className="text-lg " htmlFor="password" value="Password" />
                   <Tooltip content="Password must be at least 15 characters, include 1 uppercase, 1 number, and 1 special character from (?@!#$%^&*)." placement="right">
                     <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
                   </Tooltip>
@@ -219,16 +249,25 @@ export default function Home() {
                 {formErrors.password && <p className="text-red-600">{formErrors.password}</p>}
               </div>
 
-              <div>
-                <div className="mb-2 flex items-center">
-                  <Label className="text-lg font-semibold" htmlFor="confPass" value="Confirm Password" />
-                  <Tooltip content="Enter the same password as above." placement="right">
-                    <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
-                  </Tooltip>
+                <div>
+                  <div className="mb-2 flex items-center">
+                    <Label className="text-lg " htmlFor="confPass" value="Confirm Password" />
+                    <Tooltip content="Enter the same password as above." placement="right">
+                      <HiInformationCircle className="ml-2 text-gray-500 cursor-pointer" />
+                    </Tooltip>
+                  </div>
+                  <TextInput id="confPass" type="password" placeholder="Confirm Password" required onChange={handleChange} value={formValues.confPass} />
+                  {formErrors.confPass && <p className="text-red-600">{formErrors.confPass}</p>}
                 </div>
-                <TextInput id="confPass" type="password" placeholder="Confirm Password" required onChange={handleChange} value={formValues.confPass} />
-                {formErrors.confPass && <p className="text-red-600">{formErrors.confPass}</p>}
               </div>
+
+              {/* ------------------------------------------------------- */}
+
+
+
+
+
+
             </div>
           </div>
 
